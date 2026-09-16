3a6fd2ba4db95b70f5ebb4644ce332ebe10c1186
ablueone <vls_89@hotmail.com>
2026-09-16 23:47:31 +0200

    Styled pantry item list

--- src/components/PantryItem/PantryItem.jsx
+++ src/components/PantryItem/PantryItem.jsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
+import "./PantryItem.css";
 
 function PantryItem({ item }) {
   return (
-    <li>
-      <Link to={`/item/${item.id}`}>
-        <strong>{item.name}</strong>
-      </Link>
+    <li className="pantry-item">
+      <Link to={`/item/${item.id}`} className="pantry-item-link">
+        <div className="pantry-item-info">
+          <strong>{item.name}</strong>
+          <span>{item.category}</span>
+        </div>
 
-      <span> - {item.quantity} st</span>
+        <span className="pantry-item-quantity">
+          {item.quantity} st
+        </span>
+      </Link>
     </li>
   );
 }
