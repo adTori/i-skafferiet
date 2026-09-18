a8d29c81fb9930bf610d8b8a88cf89d08fdf4ab6
ablueone <vls_89@hotmail.com>
2026-09-18 10:05:59 +0200

    Matched add item page styling with home page

--- src/pages/AddItem/AddItem.jsx
+++ src/pages/AddItem/AddItem.jsx
@@ -1,9 +1,13 @@
 import AddItemForm from "../../components/AddItemForm/AddItemForm";
+import "./AddItem.css";
 
 function AddItem() {
   return (
-    <main>
-      <h1>Lägg till vara</h1>
+    <main className="add-item">
+      <section className="add-item-intro">
+        <h1>Lägg till vara</h1>
+        <p>Lägg till en ny vara i ditt skafferi.</p>
+      </section>
 
       <AddItemForm />
     </main>
