143dc2de57a36d8ebb8ee6cb804d8880fa03893c
ablueone <vls_89@hotmail.com>
2026-09-21 12:57:40 +0200

    Added a footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
 import Home from "./pages/Home/Home";
 import ItemDetails from "./pages/ItemDetails/ItemDetails";
 import AddItem from "./pages/AddItem/AddItem";
@@ -15,6 +17,8 @@ function App() {
         <Route path="/item/:id" element={<ItemDetails />} />
         <Route path="/add" element={<AddItem />} />
       </Routes>
+
+      <Footer />
     </>
   );
 }
